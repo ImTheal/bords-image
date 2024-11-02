68541a48eb0e30a3de722d4492e701e5a2551081
Théal <theo.moussaoui@proton.me>
2024-11-02 16:19:15 +0100

    ya

---
+++ script.js
@@ -0,0 +1,43 @@
+// URL du webhook n8n
+const webhookUrl = 'https://n8n.tambour.in/webhook/7d013591-566f-41f1-bb17-bb8ae177a449'; // Remplacez par votre URL
+document.getElementById("uploadForm").addEventListener("submit", async function(event) {
+    event.preventDefault();
+    
+    const imageInput = document.getElementById("imageInput");
+    const file = imageInput.files[0];
+    
+    if (!file) {
+        alert("Veuillez sélectionner une image à uploader.");
+        return;
+    }
+
+    const formData = new FormData();
+    formData.append("file", file); // Ajoute le fichier sous le nom "file"
+    try {
+        document.getElementById("starterbut").disabled = true;
+        fetch(webhookUrl,{
+            method: "POST",
+            body: formData
+        })
+        .then(response => response.json())
+        .then(data=>{
+            console.log(data)
+            // Afficher le message de succès
+            statusMessage.innerText = "Image envoyée avec succès !";
+            statusMessage.style.color = "green";
+            downloadImage(data.lien)
+        });
+        
+    } catch (error) {
+        console.error("Erreur lors de l'envoi de l'image :", error);
+        document.getElementById("statusMessage").innerText = "Erreur lors de l'envoi.";
+        document.getElementById("statusMessage").style.color = "red";
+    }
+});
+
+function downloadImage(source){
+    link=document.createElement('a');
+    link.href = source;
+    link.download = 'mon_image.jpg'; // Spécifiez le nom du fichier souhaité
+    link.click();
+}
